7b29bd8953e92cf2638e19cb7cdb9fef808b931a
oti <otiext@gmail.com>
2014-07-17 10:38:20 +0900

    関数化したプロパティをクォートしているのとかが無駄だったのでやめた（Androidバグ対策だった気がしてたけど気のせいだった）

--- js/hammer-drag-map.js
+++ js/hammer-drag-map.js
@@ -1,55 +1,55 @@
 // 必要な値たち
 var drag = {
-    "mapSize": {
-        "x": 1113,
-        "y": 1600
+    mapSize: {
+        x: 1113,
+        y: 1600,
     },
-    "start": {
-        "x": -556,
-        "y": -800
+    start: {
+        x: -556,
+        y: -800,
     },
-    "end": {
-        "x": -556,
-        "y": -800
+    end: {
+        x: -556,
+        y: -800,
     },
-    "max": {
-        "x": 0,
-        "y": 0
+    max: {
+        x: 0,
+        y: 0,
     },
-    "min": {
-        "x": -793,
-        "y": -1280
+    min: {
+        x: -793,
+        y: -1280,
     },
-    "transition"   : "transform .075s ease-in-out",
-    "extra"        : 25,
-    "bounce"       : true,
-    "restrict"     : true,
-    "restrictClass":{
-        "top"   : "shield__t--restrict",
-        "right" : "shield__r--restrict",
-        "bottom": "shield__b--restrict",
-        "left"  : "shield__l--restrict"
-    }
+    transition   : "transform .075s ease-in-out",
+    extra        : 25,
+    bounce       : true,
+    restrict     : true,
+    restrictClass:{
+        top   : "shield__t--restrict",
+        right : "shield__r--restrict",
+        bottom: "shield__b--restrict",
+        left  : "shield__l--restrict",
+    },
 };
 
 // 処理のかたまり
 var dragMap = {
-    "dom": {
+    dom: {
         $drag: $(".drag"),
         $tp: $(".shield__t"),
         $rt: $(".shield__r"),
         $bt: $(".shield__b"),
         $lt: $(".shield__l"),
     },
-    "view": {
-        "setMap": function(){
+    view: {
+        setMap: function(){
             // もし背景画像を JS 側で操作したい場合はここに書くといいかも
             dragMap.dom.$drag.css({
                 "width" : drag.mapSize.x,
                 "height": drag.mapSize.y
             });
         },
-        "translate": function(_pos, _transitions){
+        translate: function(_pos, _transitions){
             // マップ座標移動
             // 実行場所によって _pos は drag.start だったり drag.end だったりする
 
@@ -60,7 +60,7 @@ var dragMap = {
                 "transform" : "translate3d("+ _pos.x +"px, "+ _pos.y +"px, 0)"
             });
         },
-        "removeRestrict": function(){
+        removeRestrict: function(){
             // ドラッグ限界のクラスを消すだけの清掃業者
             dragMap.dom.$tp.removeClass(drag.restrictClass.top);
             dragMap.dom.$rt.removeClass(drag.restrictClass.right);
@@ -68,27 +68,27 @@ var dragMap = {
             dragMap.dom.$lt.removeClass(drag.restrictClass.left);
         }
     },
-    "ctrl": {
-        "dragEventsOn": function(){
+    ctrl: {
+        dragEventsOn: function(){
             // 対象要素に hammer.js を適応
             dragMap.dom.$drag.hammer().on({
                 "scroll"   : dragMap.ctrl.preventEv,
                 "touchmove": dragMap.ctrl.preventEv,
                 "dragstart": dragMap.ctrl.dragStart,
                 "drag"     : dragMap.ctrl.dragging,
-                "dragend"  : dragMap.ctrl.dragEnd
+                "dragend"  : dragMap.ctrl.dragEnd,
             });
         },
-        "preventEv": function(ev){
+        preventEv: function(ev){
             // .drag を触ってる間は画面をスクロールさせない
             ev.preventDefault();
         },
-        "dragStart": function(ev){
+        dragStart: function(ev){
             // ドラッグ開始時に .drag 内のイベントを発火させない
             ev.preventDefault();
             dragMap.view.translate(drag.start, "none");
         },
-        "dragging": function(ev){
+        dragging: function(ev){
             // ぐりぐり動かす時に初期化
             drag.start.x = drag.end.x;
             drag.start.y = drag.end.y;
@@ -107,7 +107,7 @@ var dragMap = {
             // グリグリしてる時は transition なし
             dragMap.view.translate(drag.start, "none");
         },
-        "dragEnd": function(ev){
+        dragEnd: function(ev){
             // 動かし終わった時の値を取っておく
             drag.end.x = drag.start.x;
             drag.end.y = drag.start.y;
@@ -126,7 +126,7 @@ var dragMap = {
             // transition つきで再配置
             dragMap.view.translate(drag.end, drag.transition);
         },
-        "bounce": function(drag){
+        bounce: function(drag){
             // オーバードラッグからの復帰
             // オーバードラッグの大きさは drag.extra で指定
 
@@ -150,7 +150,7 @@ var dragMap = {
                 drag.end.x = drag.max.x;
             }
         },
-        "restrict": function(drag){
+        restrict: function(drag){
             // ドラッグ量の制限
             // bounce が true だった時、extra 分のオーバードラッグを許容する
 
@@ -186,7 +186,7 @@ var dragMap = {
                 dragMap.dom.$lt.removeClass(drag.restrictClass.left);
             }
         },
-        "init": function(){
+        init: function(){
             // マップのサイズを設定
             dragMap.view.setMap();
 
